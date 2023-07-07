444ee826e67ee53f738d29969a96e48f9730083e
Stephen Zhou <hi@hyoban.cc>
2023-07-07 11:09:12 +0800

    fix(desktop): schema

--- packages/desktop/src/schema/index.ts
+++ packages/desktop/src/schema/index.ts
@@ -3,7 +3,7 @@ import { z } from "zod"
 export const OPEN_API_SCHEMA = z
 	.string()
 	.url()
-	.regex(/^(http|https):\/\/.*\/api\/v1\/memo\?openId=[a-zA-Z0-9]*/, {
+	.regex(/^(http|https):\/\/.*\/api.*\/memo\?openId=[a-zA-Z0-9]*/, {
 		message: "OpenAPI url is invalid",
 	})
 
